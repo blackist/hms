4fb6402e3d65714a1310c54fb802365823cb23e7
CQ <746282024@qq.com>
2016-01-07 17:33:48 +0800

    mediInfo package

---
+++ HMS/src/com/xzit/hms/dao/mediinfo/impl/MediInfoDao.java
@@ -0,0 +1,8 @@
+package com.xzit.hms.dao.mediinfo.impl;
+
+import com.xzit.hms.bean.medicine.MediInfo;
+import com.xzit.hms.dao.impl.BaseDaoImpl;
+
+public class MediInfoDao extends BaseDaoImpl<MediInfo> {
+
+}
