65749fd2c17cd3db99cb20d5fe3e39ce41a8819b
DLL <1075512174@qq.com>
2016-01-20 11:18:45 +0800

    Dlll

--- HMS/WebContent/WEB-INF/jsp/side-bar/bar-operation.jsp
+++ HMS/WebContent/WEB-INF/jsp/side-bar/bar-operation.jsp
@@ -5,7 +5,7 @@
 		class="am-icon-file"></span> 手术管理 <span
 		class="am-icon-angle-right am-fr am-margin-right"></span></a>
 	<ul class="am-list am-collapse admin-sidebar-sub" id="bar-operation">
-		<li><a href="user/indexUser.action" class="am-cf"><span
+		<li><a href="operation/indexOper.action" class="am-cf"><span
 				class="am-icon-check"></span> 手术清单 </a></li>
 		<li><a href="admin-help.html"><span
 				class="am-icon-puzzle-piece"></span> 手术结果</a></li>
---
+++ HMS/WebContent/assets/script/inhospital/operation/user-query.js
@@ -0,0 +1,32 @@
+$(function() {
+	/* 绑定查询函数 */
+	_pageIndexBond(queryUsers);
+});
+/*查询用户*/
+function queryUsers(pagecode) {
+	var queryStr = $("#queryStr").val();
+	$.post("user/queryUsers.action", {"pagecode":pagecode,"queryStr":queryStr}, function(data) {
+		$("#admin-content").html(data);
+	});
+}
+/*添加用户*/
+function addUser(){
+	$("#admin-content").load("user/addUser.action");
+}
+/*删除用户*/
+function deleteUser(id){
+	$.post("user/deleteUser.action", {"userId":id}, function(data) {
+		$("#admin-content").html(data);
+	});
+}
+/*更新用户*/
+function updateUser(id){
+	$.post("user/updateUser.action", {"userId":id}, function(data){
+		$("#admin-content").html(data);
+	});
+}
+
+
+
+
+
